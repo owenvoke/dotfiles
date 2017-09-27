15a88d626217a29c4393d48fb2f90f3858698f7b
pxgamer <owzie123@gmail.com>
2017-09-27 13:01:35 +0100

    Updated method of downloading composer signature to use cURL as it's faster

--- development/composer.sh
+++ development/composer.sh
@@ -1,4 +1,4 @@
-EXPECTED_SIGNATURE=$(php -r "echo file_get_contents('https://composer.github.io/installer.sig');")
+EXPECTED_SIGNATURE=$(curl -sS https://composer.github.io/installer.sig)
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
 
