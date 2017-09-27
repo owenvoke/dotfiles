fd623cea8004bf10d402d14b01516779059b2ee1
pxgamer <owzie123@gmail.com>
2017-09-27 14:50:19 +0100

    Removed diff echo for install/composer.sh

--- install/composer.sh
+++ install/composer.sh
@@ -2,8 +2,6 @@ EXPECTED_SIGNATURE=$(curl -sS https://composer.github.io/installer.sig)
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
 
-echo $ACTUAL_SIGNATURE
-echo $EXPECTED_SIGNATURE
 if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
 then
     >&2 echo 'ERROR: Invalid installer signature'
