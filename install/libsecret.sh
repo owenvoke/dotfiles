513a8b509fc4d67f41151cb0c45ff768bcaff873
pxgamer <owzie123@gmail.com>
2017-09-29 21:52:31 +0100

    Changed  to use a directory, rather than using cd

--- install/libsecret.sh
+++ install/libsecret.sh
@@ -2,8 +2,7 @@
 sudo apt-get install libsecret-1-0 libsecret-1-dev
 
 # Make credential-manager
-cd /usr/share/doc/git/contrib/credential/libsecret
-sudo make
+sudo make -C /usr/share/doc/git/contrib/credential/libsecret
 
 # Copy to /usr/bin
 sudo cp /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret /usr/bin/git-credential-libsecret
