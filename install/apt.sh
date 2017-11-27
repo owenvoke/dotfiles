29957bba0dc85603ad496dcb22106b8ca41668dd
pxgamer <owzie123@gmail.com>
2017-11-27 09:16:07 +0000

    Added ppa:ondrej/php

--- install/apt.sh
+++ install/apt.sh
@@ -1,7 +1,10 @@
-# Install Aptitude dependencies
+# Add PPAs
+sudo add-apt-repository ppa:ondrej/php
 
+# Install Aptitude dependencies
 sudo apt install zsh \
                          git \
 						 php \
 						 nodejs
+sudo apt install gawk
 sudo apt install ack-grep
