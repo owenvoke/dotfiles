8291f3d829eb1229ea9bf51a1d48f2b92f008ed4
pxgamer <owzie123@gmail.com>
2017-11-28 22:01:03 +0000

    Added additional php extensions

--- install/php.sh
+++ install/php.sh
@@ -4,3 +4,6 @@ sudo apt install php-mbstring -y
 sudo apt install php-bcmath -y
 sudo apt install php-json -y
 sudo apt install php-xml -y
+sudo apt install php-mcrypt -y
+sudo apt install php-gmp -y
+sudo apt install php-xdebug -y
