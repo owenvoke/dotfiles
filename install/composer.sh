675d685a6eb98c375448e7795eb3a63d86ee9cec
pxgamer <owzie123@gmail.com>
2017-11-29 09:14:45 +0000

    Changed if;then to be on single lines for consistency/readability

--- install/composer.sh
+++ install/composer.sh
@@ -5,8 +5,7 @@ EXPECTED_SIGNATURE=$(curl -sS https://composer.github.io/installer.sig)
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
 
-if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
-then
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]; then
     >&2 echo 'ERROR: Invalid installer signature'
     rm composer-setup.php
     exit 1
@@ -21,10 +20,8 @@ sudo mv composer.phar /usr/bin/composer
 
 # Install Composer dependencies
 /usr/bin/composer global require squizlabs/php_codesniffer
-if [ -d "$HOME/.composer/vendor/bin" ]
-then
+if [ -d "$HOME/.composer/vendor/bin" ]; then
     $HOME/.composer/vendor/bin/phpcs --config-set default_standard PSR2
-elif [ -d "$HOME/.config/composer/bin" ]
-then
+elif [ -d "$HOME/.config/composer/bin" ]; then
     $HOME/.config/composer/vendor/bin/phpcs --config-set default_standard PSR2
 fi
