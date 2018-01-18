940df73ab4161af82eb01f6a03d6a85def4aac62
pxgamer <owzie123@gmail.com>
2018-01-18 22:38:55 +0000

    Added OSX tests

---
+++ tests/osx-tests.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+testBrewIsInstalled() {
+	assertEquals "/usr/local/bin/brew" "$(which brew)"
+}
+
+testZshIsDefaultShell() {
+	assertEquals "$(which zsh)" "$SHELL"
+}
+
+testZplugIsInstalled() {
+	assertTrue "[ -d '${HOME}/.zplug' ]"
+}
+
+testSymlinksHaveBeenCreated() {
+	assertTrue "[ -f '${HOME}/.gitconfig' ]"
+	assertTrue "[ -f '${HOME}/.gitconfig-work' ]"
+	assertTrue "[ -f '${HOME}/.gitignore_global' ]"
+	assertTrue "[ -f '${HOME}/.zprofile' ]"
+	assertTrue "[ -f '${HOME}/.bash_profile' ]"
+	assertTrue "[ -f '${HOME}/.zshrc' ]"
+	assertTrue "[ -f '${HOME}/.gnupg/gpg.conf' ]"
+}
+
+source ../shunit2-2.1.7/shunit2
