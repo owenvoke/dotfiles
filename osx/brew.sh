c26c06150be907bdad0836281ec66d399ac42001
pxgamer <owzie123@gmail.com>
2018-01-18 15:59:39 +0000

    Added osx-specific installers

---
+++ osx/brew.sh
@@ -0,0 +1,12 @@
+# Brew updates
+brew doctor
+brew update
+
+# Brew taps
+brew tap homebrew/homebrew-php
+
+# Install Brew dependencies
+brew install zsh
+brew install php72
+brew install zplug
+brew install gawk
