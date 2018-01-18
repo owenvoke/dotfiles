3218cf99467ed2a1bacf073f8853748e563daa97
pxgamer <owzie123@gmail.com>
2018-01-18 22:35:39 +0000

    Added linux tests

---
+++ tests/linux-tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+testZshIsDefaultShell() {
+	assertEquals "$(which zsh)" "$SHELL"
+}
+
+testZplugIsInstalled() {
+	assertTrue "[ -d '${HOME}/.zplug' ]"
+}
+
+testSymlinksHaveBeenCreated() {
+	assertTrue "[ -f '${HOME}/.gitconfig' ]"
+	assertTrue "[ -f '${HOME}/.gitconfig-work' ]"
+	assertTrue "[ -f '${HOME}/.gitignore_global' ]"
+	assertTrue "[ -f '${HOME}/.zprofile' ]"
+	assertTrue "[ -f '${HOME}/.bash_profile' ]"
+	assertTrue "[ -f '${HOME}/.zshrc' ]"
+	assertTrue "[ -f '${HOME}/.gnupg/gpg.conf' ]"
+}
+
+source ../shunit2-2.1.7/shunit2
